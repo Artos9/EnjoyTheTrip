e9aa55314dbbfdaaf38a9e8010e51083b4114db3
Lukasz_Krol <Lukaskrol93@gmail.com>
2020-04-23 00:18:05 +0200

    Laravel init and frontend template integration

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontend.index');
+});
+
+Route::get('/', 'FrontendController@index')->name('home');
+
+Route::get('/obiekt', 'FrontendController@object')->name('object');
+Route::get('/adminHome', 'FrontendController@adminHome')->name('adminHome');
+Route::get('/roomSearch', 'FrontendController@roomSearch')->name('roomSearch');
